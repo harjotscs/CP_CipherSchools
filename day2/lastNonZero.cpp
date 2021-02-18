76996d5e492b82cfaf39ae073af4db81e85bf047
harjotscs <harjotscs@gmail.com>
2021-02-18 23:51:40 +0530

    added day2

---
+++ day2/lastNonZero.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+int dig[] = {2, 1, 2, 5, 4, 2, 1, 4, 2, 6}; 
+
+int lastNon0Digit(int n) 
+{ 
+	if (n < 10) 
+		return dig[n]; 
+	if (((n/10)%10)%2 == 0) 
+		return (6*lastNon0Digit(n/5)*dig[n%10]) % 10; 
+	else
+		return (4*lastNon0Digit(n/5)*dig[n%10]) % 10; 
+} 
+
+int main() 
+{ 
+	int n = 3; 
+	cout << lastNon0Digit(n); 
+	return 0; 
+} 
---
+++ day2/possiblePath.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int numberOfPaths(int m, int n) 
+{ 
+    
+	int path = 1; 
+	for (int i = n; i < (m + n - 1); i++) { 
+		path *= i; 
+		path /= (i - n + 1); 
+	} 
+	return path; 
+} 
+
+int main() 
+{ 
+	cout << numberOfPaths(6, 4); 
+	return 0; 
+}
